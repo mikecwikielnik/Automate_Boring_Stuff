3ed954903b83956411fedec09e41d1915d72f6b3
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-05-14 21:42:57 -0400

    Add files via upload

---
+++ Part I/Ch 2/Questions/2_13q.py
@@ -0,0 +1,19 @@
+"""
+Question 13, Chapter 2
+
+13. Write a short program that prints the numbers 1 to 10 using a for loop. 
+Then write an equivalent program that prints the numbers 1 to 10 using a while loop.
+
+Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 56). No Starch Press. Kindle Edition. 
+"""
+
+# The for loop
+for i in range(1, 11):
+    print(i)
+    
+# The while loop
+
+n = 1
+while n < 11:
+    print(n)
+    n = n + 1
