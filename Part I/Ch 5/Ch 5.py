b693b5fd0b4a2ef16c8a4b1bdcea210d711b79f7
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-05-18 18:36:28 -0400

    Add files via upload

--- Part I/Ch 5/Ch 5.py
+++ Part I/Ch 5/Ch 5.py
@@ -81,3 +81,32 @@ if 'color' not in spam: # add the below key, value
     spam['color'] = 'black'
 
 spam.setdefault('color', 'green')
+
+# Nested dictioinaries and lists
+
+allGuests = {
+    'Alice': {
+        'apples': 5,
+        'pretzels': 12
+    },
+    'Bob': {
+        'sandwiches': 3,
+        'apples': 2
+    },
+    'Carol': {
+        'cups': 3,
+        'apple pies': 1
+    }
+}
+def totalBrought(guests, item):
+    numBrought = 0
+    for k, v in guests.items():
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
+
+print('Number of things being brought:')
+print(' - Apples        ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups          ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes         ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Sandwiches    ' + str(totalBrought(allGuests, 'sandwiches')))
+print(' - Apple Pies    ' + str(totalBrought(allGuests, 'apple pies')))
