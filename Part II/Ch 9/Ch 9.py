479a93d8ea900b6011a23d30310b90f72a76fbb8
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-06-16 12:32:26 -0400

    Add files via upload

--- Part II/Ch 9/Ch 9.py
+++ Part II/Ch 9/Ch 9.py
@@ -6,6 +6,7 @@ Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 201). No St
 
 from fileinput import hook_encoded
 from pathlib import Path
+from turtle import home
 
 Path('spam', 'bacon', 'eggs')
 
@@ -23,3 +24,26 @@ homeFolder = r'C:\Users\mikec'
 subFolder = 'spam'
 homeFolder + '\\' + subFolder
 '\\'.join([homeFolder, subFolder])
+
+
+homeFolder = Path('C:/Users/mikec')
+subFolder = Path('spam')
+homeFolder / subFolder
+str(homeFolder / subFolder)
+
+# The Current Working Directory
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 205). No Starch Press. Kindle Edition. 
+
+from pathlib import Path
+
+import os
+
+Path.cwd()  # same as os.getcwd()
+os.getcwd() # except, os.getcwd() output is a string 
+
+# The Home Directory
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 206). No Starch Press. Kindle Edition. 
+
+Path.home()
