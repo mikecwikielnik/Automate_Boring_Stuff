7de6afc0b3d125be8b4657cffc4bd04d5296ea7a
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-06-20 14:33:24 -0400

    Add files via upload

--- Part II/Ch 9/Ch 9.py
+++ Part II/Ch 9/Ch 9.py
@@ -6,6 +6,7 @@ Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 201). No St
 
 from fileinput import hook_encoded
 from pathlib import Path
+from struct import calcsize
 from turtle import home
 
 Path('spam', 'bacon', 'eggs')
@@ -47,3 +48,80 @@ os.getcwd() # except, os.getcwd() output is a string
 # Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 206). No Starch Press. Kindle Edition. 
 
 Path.home()
+
+# Handling Absolute and Relative Paths
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 208). No Starch Press. Kindle Edition. 
+
+Path.cwd()
+Path.cwd().is_absolute()
+Path('spam/bacon/eggs').is_absolute()
+
+Path('my/relative/path')
+Path.cwd()/Path('my/relative/path')
+
+# To get to another folder but starting at the C: 
+
+Path.home()/Path('my/relative/path')
+
+# Getting the Parts of a File Path
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 209). No Starch Press. Kindle Edition. 
+
+p = Path('C:/Users/Al/spam.txt')
+p.anchor
+p.parent    # This is a Path object, not a string.
+p.name
+p.stem
+p.suffix
+p.drive
+
+Path.cwd()
+Path.cwd().parents[0]
+Path.cwd().parents[2]
+Path.cwd().parents[5]
+
+
+
+calcFilePath = 'C:\\Windows\\System32\\calc.exe'
+os.path.basename(calcFilePath)
+os.path.dirname(calcFilePath)
+
+os.path.split(calcFilePath)
+
+(os.path.dirname(calcFilePath), os.path.basename(calcFilePath))
+
+# Returns specific folders
+
+calcFilePath.split(os.sep)
+
+# Finding File Sizes and Folder Contents
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 212). No Starch Press. Kindle Edition. 
+
+os.path.getsize('C:\\Windows\\System32\\calc.exe')
+
+os.listdir('C:\\Windows\\System32')
+
+totalsize = 0
+
+for filename in os.listdir('C:\\Windows\\System32'):
+    totalsize = totalsize + os.path.getsize(os.path.join('C:\\Windows\\System32', filename))
+print(totalsize)
+
+# Modifying a List of Files Using Glob Patterns
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 213). No Starch Press. Kindle Edition. 
+
+p = Path('c:/Users/mikec/Desktop')
+p.glob('*')
+list(p.glob('*'))   # Make a list froma the generator
+
+list(p.glob('*.txt'))   # Lists all text files
+
+list(p.glob('*.?x?'))
+
+p = Path('C:/Users/mikec/Desktop')
+for textFilePathObj in p.glob('*.txt'):
+    print(textFilePathObj)  # Prints the Path object as a string
+    
