b8bfededb5f675df3bbf55f20fd1d38758b30c3b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-06-28 00:42:28 -0400

    Add files via upload

---
+++ Part II/Ch 10/Project Renaming Files with American-Style Dates to European-Style Dates/renameDates.py
@@ -0,0 +1,6 @@
+"""
+Project: Renaming Files with American-Style Dates to European-Style Dates
+
+Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 240). No Starch Press. Kindle Edition. 
+"""
+
