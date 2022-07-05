8698a6696e3a82ff53615c2344ad3fb5e50dd482
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-07-05 17:54:45 -0400

    Add files via upload

--- Part II/Ch 11/buggyAddingProgram.py
+++ Part II/Ch 11/buggyAddingProgram.py
@@ -3,3 +3,12 @@ buggyAddingProgram.py
 
 Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 262). No Starch Press. Kindle Edition. 
 """
+
+print('Enter the first number to add:')
+first = input()
+print('Enter the second number to add:')
+second = input()
+print('Enter the third number to add:')
+third = input()
+print('The sum is ' + first + second + third)
+
