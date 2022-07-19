c840de3b13fee6f03d27d250f1dbdecc67f7e621
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-07-18 20:53:46 -0400

    Add files via upload

--- Part II/Ch_16/Ch_16.py
+++ Part II/Ch_16/Ch_16.py
@@ -13,6 +13,7 @@ Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 371). No St
 # Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 373). No Starch Press. Kindle Edition. 
 
 import csv
+from hashlib import new
 
 exampleFile = open('example.csv')
 exampleReader = csv.reader(exampleFile)
@@ -38,3 +39,57 @@ exampleReader = csv.reader(exampleFile)
 for row in exampleReader:
     print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
     
+# writer Objects
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 374). No Starch Press. Kindle Edition. 
+
+import csv
+
+outputFile = open('output.csv', 'w', newline='')
+outputWriter = csv.writer(outputFile)
+outputWriter.writerow(['spam', 'eggs', 'bacon', 'ham'])
+outputWriter.writerow(['Hello, world!', 'eggs', 'bacon', 'ham'])
+outputWriter.writerow([1,2,3.141592,4])
+outputFile.close()
+
+# The delimiter and lineterminator Keyword Arguments
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 375). No Starch Press. Kindle Edition. 
+
+import csv
+
+csvFile = open('example.tsv', 'w', newline='')
+csvWriter = csv.writer(csvFile, delimiter='\t', lineterminator='\n\n')
+csvWriter.writerow(['apples', 'oranges', 'grapes'])
+csvWriter.writerow(['eggs', 'bacon', 'ham'])
+csvWriter.writerow(['spam', 'spam', 'spam', 'spam', 'spam', 'spam'])
+csvFile.close()
+
+# With Headers
+
+import csv
+
+exampleFile = open('exampleWithHeader.csv')
+exampleDictReader = csv.DictReader(exampleFile)
+for row in exampleDictReader:
+    print(row['Timestamp'], row['Fruit'], row['Quantity'])
+
+# Without Header names
+
+import csv
+
+exampleFile = open('example.csv')
+exampleDictReader = csv.DictReader(exampleFile, ['time', 'name',
+                                                 'amount'])
+for row in exampleDictReader:
+    print(row['time'], row['name'], row['amount'])
+    
+import csv
+
+outputFile = open('output.csv', 'w', newline='')
+outputDictWriter = csv.DictWriter(outputFile, ['Name', 'Pet', 'Phone'])
+outputDictWriter.writeheader()
+outputDictWriter.writerow({'Name': 'Alice', 'Pet': 'cat', 'Phone':'555-1234'})
+outputDictWriter.writerow({'Name': 'bob', 'Phone': '555-9999'})
+outputDictWriter.writerow({'Phone': '555-5555', 'Name': 'Carol', 'Pet': 'dog'})
+outputFile.close()
