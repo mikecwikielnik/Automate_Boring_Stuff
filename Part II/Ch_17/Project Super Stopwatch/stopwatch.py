4a8b18da72643eec947d58fe309814916a5b986d
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-07-19 19:56:52 -0400

    Add files via upload

---
+++ Part II/Ch_17/Project Super Stopwatch/stopwatch.py
@@ -0,0 +1,32 @@
+"""
+stopwatch.py
+
+Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 393). No Starch Press. Kindle Edition. 
+"""
+
+#!
+# stopwatch.py - A simple stopwatch program
+
+import time
+
+# Display the program's instructions
+print('Press ENTER to begin. Afterward, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
+input() # press Enter to begin
+print('Started.')
+startTime = time.time() # get the first lap's start time
+lastTime = startTime
+lapNum = 1
+
+# Start tracking the lap times
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
+        lapNum += 1
+        lastTime = time.time()  # reset teh last lap time
+except KeyboardInterrupt:
+    # Handle the Ctrl-C exception to keep its error message from displaying
+    print('\nDone.')
+    
