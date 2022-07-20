c2562ddcfe88f316fd6c8d478cf7dae75630892c
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2022-07-19 20:59:00 -0400

    Add files via upload

--- Part II/Ch_17/ch_17.py
+++ Part II/Ch_17/ch_17.py
@@ -10,6 +10,8 @@ Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 389). No St
 
 import time
 
+from django.forms import DateInput
+
 time.time()
 
 import time
@@ -56,3 +58,78 @@ now
 round(now, 2)
 round(now, 4)
 round(now)
+
+# The datetime Module
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 395). No Starch Press. Kindle Edition. 
+
+import datetime
+
+datetime.datetime.now()
+dt = datetime.datetime(2019, 10, 21, 16, 29, 0)
+dt.year, dt.month, dt.day
+dt.hour, dt.minute, dt.second
+
+import datetime, time
+
+datetime.datetime.fromtimestamp(10000)
+datetime.datetime.fromtimestamp(time.time())
+
+halloween2019 = datetime.datetime(2019, 10, 31, 0, 0, 0)
+newyears2020 = datetime.datetime(2020, 1, 1, 0, 0, 0)
+oct31_2019 = datetime.datetime(2019, 10, 31, 0, 0, 0)
+halloween2019 == oct31_2019
+halloween2019 > newyears2020
+newyears2020 > halloween2019
+newyears2020 != oct31_2019
+
+# The timedelta Data Type
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 396). No Starch Press. Kindle Edition. 
+
+delta = datetime.timedelta(days=11, hours=10, minutes=9, seconds=8)
+delta.days, delta.seconds, delta.microseconds
+delta.total_seconds()
+str(delta)
+
+dt = datetime.datetime.now()
+dt 
+thousandDays = datetime.timedelta(days=1000)
+dt + thousandDays
+
+oct21st = datetime.datetime(2019, 10, 21, 16, 29, 0)
+aboutThirtyYears = datetime.timedelta(days=365 * 30)
+oct21st
+oct21st - aboutThirtyYears
+oct21st - (2 * aboutThirtyYears)
+
+# Pausing Until a Specific Date
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 397). No Starch Press. Kindle Edition. 
+
+import datetime
+import time
+
+halloween2016 = datetime.datetime(2016, 10, 31, 0, 0, 0)
+while datetime.datetime.now() < halloween2016:
+    time.sleep(1)
+    
+# Converting datetime Objects into Strings
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 398). No Starch Press. Kindle Edition. 
+
+oct21st = datetime.datetime(2019, 10, 21, 16, 29, 0)
+oct21st.strftime('%Y/%m/%d %H:%M:%S')
+oct21st.strftime('%I:%M %p')
+oct21st.strftime("%B of '%y")   # notice small y
+
+# Converting Strings into datetime Objects
+
+# Sweigart, Al. Automate the Boring Stuff with Python, 2nd Edition (p. 399). No Starch Press. Kindle Edition. 
+
+datetime.datetime.strptime('October 21, 2019', '%B %d, %Y')
+datetime.datetime.strptime('2019/10/21 16:29:00', '%Y/%m/%d %H:%M:%S')
+datetime.datetime.strptime("October of '19", "%B of '%y")   # notice small y
+datetime.datetime.strptime("November of '63", "%B of '%y")
+
+
